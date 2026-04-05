487f80213d9c73f1277fc2f2d4f74cc075e70a00
Tarunvir <TarunvirBains@kindnudge.app>
2026-04-05 03:56:54 -0600

    refactor(python): remove Django code from core, clean up pyproject.toml
    Django fields, apps, and migrations now live in the separate
    heeranjid-django package. Core package only provides types and SQL constants.
    
    please visit https://kindnudge.app/

--- bindings/python/python/heeranjid/django/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class HeeranjidConfig(AppConfig):
-    name = "heeranjid.django"
-    label = "heeranjid"
-    verbose_name = "HeeRanjID"
-    default_auto_field = "django.db.models.BigAutoField"
